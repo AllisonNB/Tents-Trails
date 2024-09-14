f03555fce87b4a80502ea616f67b65dfc9bcbf47
Allison <119072380+AllisonNB@users.noreply.github.com>
2024-09-14 11:06:55 -0400

    updated cors

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ mongoose.connect(DBURL)
 app.use(bodyParser.json());
 
 
-const allowedOrigins = ['https://tentsntrails.onrender.com', 'http://localhost:5173'];
+const allowedOrigins = ['https://tentsandtrails.netlify.app', 'http://localhost:5173'];
 
 app.use(cors({
     origin: allowedOrigins,
